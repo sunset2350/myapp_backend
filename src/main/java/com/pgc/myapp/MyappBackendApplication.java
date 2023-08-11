a54dc1fe3d74b63cea0b234dd0d2f8a005faa0db
GangChan Park <sunset2350@naver.com>
2023-08-11 12:09:32 +0900

    first commit

---
+++ src/main/java/com/pgc/myapp/MyappBackendApplication.java
@@ -0,0 +1,15 @@
+package com.pgc.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappBackendApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/pgc/myapp/MyappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgc.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
